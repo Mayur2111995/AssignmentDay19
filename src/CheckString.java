4fff4e75c335b2793a557fa3e9c31b05198921cd
Mayur2111995 <barimayur1265@gmail.com>
2022-07-14 08:10:23 +0530

    Added:Regex java regular Expression

---
+++ src/CheckString.java
@@ -0,0 +1,17 @@
+import java.util.regex.*;
+
+    public class CheckString
+    {
+        public boolean checkPresentString() {
+
+            System.out.println(Pattern.matches("[gert]", "gert"));
+            System.out.println(Pattern.matches("[gert]*", "terge"));
+            System.out.println(Pattern.matches("[aabb]", "baab"));
+            return false;
+        }
+
+        public static void main(String[] args) {
+            CheckString s = new CheckString();
+            s. checkPresentString();
+        }
+    }
